12991031b3df3c43aa27d8b09864ce9850454fae
sudips413 <sudips413@gmsil.com>
2023-03-25 15:54:38 +0545

    setting was updated

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const port = 3000;
 const survey = require('./Model/Survey');
+//for converting the documents to csv
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+    path: 'data.csv',
+    header: [
+        {id: 'name', title: 'Name'},
+        {id: 'age', title: 'Age'},
+        {id:'sex',title:"Sex"},
+        {id:'platform',title:"Platform"},
+        {id:'frequency',title:"Frequency"}
+    ]
+});
+
 
 //connect to mongodb 
 const mongoose = require('mongoose');
@@ -24,7 +37,6 @@ app.listen(port, () => {
 
 //route to handle post request
 app.post('/survey', async(req, res) => {
-    console.log(req.body);
     const obj = new survey({
         name: req.body.name,
         age: req.body.age,
@@ -34,7 +46,7 @@ app.post('/survey', async(req, res) => {
     });
     await obj.save().then((data)=>{
         res.status(200).send('Data saved successfully');
-        console.log(data);
+        
     })
     .catch(err => {
         res.status(400).send('Unable to save to database');
@@ -47,14 +59,17 @@ app.post('/survey', async(req, res) => {
 
 //get all the survey data
 app.get('/data', async(req, res) => {
-    await survey.find().then((data)=>{
-        res.status(200).send(data);
-    })
-    .catch(err => {
-        res.status(400).send('Unable to fetch data');
-        console.log(err);
+    try {
+        const data = await survey.find();
+        await csvWriter.writeRecords(data);
+        const csvFile = `${__dirname}/data.csv`;
+        res.setHeader('Content-Type', 'text/csv');
+        res.setHeader('Content-Disposition', 'attachment; filename="' + 'data.csv' + '"');
+        res.download(csvFile);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
     }
-    );  
 });
 
 
